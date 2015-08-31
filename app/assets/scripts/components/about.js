2b066a0b992e35159405826e5f4bd1f926d908af
Joe Flasher <joseph.flasher@gmail.com>
2015-08-31 08:29:46 -0400

    Reworking for about page
    closes #13

--- app/assets/scripts/components/about.js
+++ app/assets/scripts/components/about.js
@@ -1,8 +1,22 @@
 var React = require('react');
+var ReactIntl = require('react-intl');
+var MdastComponent = require('mdast-react-component');
 
 var Header = require('./header');
 
+var IntlMixin = ReactIntl.IntlMixin;
+var FormattedMessage = ReactIntl.FormattedMessage;
+
 var About = React.createClass({
+
+  mixins: [
+    IntlMixin,
+  ],
+
+  propTypes: {
+    messages: React.PropTypes.object.isRequired,
+    locales: React.PropTypes.array.isRequired
+  },
 
   propTypes: {
     messages: React.PropTypes.object.isRequired,
@@ -13,7 +27,11 @@ var About = React.createClass({
     return (
       <div>
         <Header locales={this.props.locales} messages={this.props.messages} style='dark' />
-        <div>About Page</div>
+        <div className='about page'>
+          <div className='intro'>
+            <MdastComponent>{this.getIntlMessage('about.intro')}</MdastComponent>
+          </div>
+        </div>
       </div>
     );
   }
--- app/assets/scripts/i18n/en/index.js
+++ app/assets/scripts/i18n/en/index.js
@@ -15,13 +15,15 @@ var i18n = {
         footer: '{number, plural, =0 {Made} =1 {# measurement captured} other {# measurements captured}} with {heart} by {link}.',
         sources: {
           addData: 'Suggest a Source'
-        }
+        },
+        about: {}
     }
 };
 
 /**
  * Probably best not to edit below here
  */
-i18n.messages.sources.intro = fs.readFileSync(__dirname + '/sourcesIntro.md', 'utf8');
+i18n.messages.sources.intro = fs.readFileSync(__dirname + '/sources/intro.md', 'utf8');
+i18n.messages.about.intro = fs.readFileSync(__dirname + '/about/intro.md', 'utf8');
 
 module.exports = i18n;
