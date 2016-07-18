e9c1924d778f415f114d504f3a83905be3e57b25
Daniel da Silva <daniel@developmentseed.org>
2016-07-18 18:19:02 -0400

    Add footer

--- app/assets/scripts/components/page-footer.js
+++ app/assets/scripts/components/page-footer.js
@@ -11,7 +11,25 @@ var PageFooter = React.createClass({
     return (
       <footer className='page__footer' role='contentinfo'>
         <div className='inner'>
-          <p>Made with love by <a href='https://developmentseed.org' title='Visit Development Seed website'>Development Seed</a> and <a href='https://openaq.org/' title='Visit the OpenAQ website'>OpenAQ</a>.</p>
+          <div className='contact-info'>
+            <div className='contact contact--social'>
+              <h2 className='contact__title'>Connect with us</h2>
+              <ul>
+                <li><a href='' className='contact-button-gh' title='View Github'><span>Github</span></a></li>
+                <li><a href='' className='contact-button-slack' title='View Slack'><span>Slack</span></a></li>
+                <li><a href='' className='contact-button-twitter' title='View Twitter'><span>Twitter</span></a></li>
+                <li><a href='' className='contact-button-mail' title='View Email'><span>Email</span></a></li>
+              </ul>
+            </div>
+            <div className='contact contact--newsletter'>
+              <h2 className='contact__title'>Subscribe to our newsletter</h2>
+              <div className='form__input-group'>
+                <input type='text' placeholder='bruce@wayne.com' className='form__control form__control--medium' />
+                <span className='form__input-group-button'><button className='button--subscribe' type='submit'><span>Subscribe</span></button></span>
+              </div>
+            </div>
+          </div>
+          <p className='copyright'>13,735,288 measurements captured with love by <a href='https://developmentseed.org' title='Visit Development Seed website'>Development Seed</a> and the <a href='https://openaq.org/' title='Visit the OpenAQ website'>OpenAQ</a> team.</p>
         </div>
       </footer>
     );
