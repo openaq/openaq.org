6aa9899718e0543c32021ff59e6181c93eda49d4
Daniel da Silva <daniel@developmentseed.org>
2016-07-19 16:24:26 -0400

    Style location page

---
+++ app/assets/scripts/components/location-card.js
@@ -0,0 +1,39 @@
+'use strict';
+import React from 'react';
+
+var LocationCard = React.createClass({
+  displayName: 'LocationCard',
+
+  propTypes: {
+  },
+
+  render: function () {
+    // card--data-compact
+    return (
+      <article className='card'>
+        <div className='card__contents'>
+          <header className='card__header'>
+            <p className='card__subtitle'>Updated <strong>1 hour ago</strong></p>
+            <h1 className='card__title'><a href='#' title='View Wollogong page'>Wollogong</a> in Illawara, Australia</h1>
+          </header>
+          <div className='card__body'>
+            <ul className='card__meta-details'>
+              <li>Collection Start: 2015/03/01</li>
+              <li>Measurements: 30,665</li>
+              <li>Values: PM2.5, PM10, O3</li>
+              <li>Source: <a href='#' title='View source for Wollogong'>EPA</a></li>
+            </ul>
+          </div>
+          <footer className='card__footer'>
+            <ul className='card__actions'>
+              <li><a href='#' className='button-card-download' title='Download data for Wollogong'>Download</a></li>
+              <li><a href='#' className='button-card-view' title='View Wollogong page'>View More</a></li>
+            </ul>
+          </footer>
+        </div>
+      </article>
+    );
+  }
+});
+
+module.exports = LocationCard;
--- app/assets/scripts/views/locations-hub.js
+++ app/assets/scripts/views/locations-hub.js
@@ -1,6 +1,9 @@
 'use strict';
 import React from 'react';
 import { connect } from 'react-redux';
+import { ScrollArea, Dropdown } from 'openaq-design-system';
+import ReactPaginate from 'react-paginate';
+import LocationCard from '../components/location-card';
 
 var LocationsHub = React.createClass({
   displayName: 'LocationsHub',
@@ -26,6 +29,118 @@ var LocationsHub = React.createClass({
           </div>
         </header>
         <div className='inpage__body'>
+          <div className='inner full-flex'>
+
+            <aside className='inpage__aside'>
+              <h2 className='content-prime-title'>Filter Locations</h2>
+
+              <div className='filters filters--country'>
+                <h3 className='filters__title'>Countries</h3>
+
+                  <ScrollArea
+                    speed={0.8}
+                    className='filters__group'
+                    contentClassName='filters__group-inner'
+                    smoothScrolling={true}
+                    horizontal={false} >
+
+                    {[0, 0, 0, 0, 0, 0, 0].map(o => {
+                      return (
+                        <label className='form__option form__option--custom-checkbox'>
+                          <input type='checkbox' value='Checkbox 1' id='form-checkbox-1' name='form-checkbox' />
+                          <span className='form__option__text'>Checkbox 1</span>
+                          <span className='form__option__ui'></span>
+                        </label>
+                      );
+                    })}
+                  </ScrollArea>
+
+              </div>
+
+              <div className='filters filters--values'>
+                <h3 className='filters__title'>Values</h3>
+                <div className='filters__group'>
+
+                  <ScrollArea
+                    speed={0.8}
+                    className='filters__group'
+                    contentClassName='filters__group-inner'
+                    smoothScrolling={true}
+                    horizontal={false} >
+
+                    {['PM2.5', 'PM10', 'O3', 'SO2', 'NO2', 'CO', 'BC'].map(o => {
+                      return (
+                        <label className='form__option form__option--custom-checkbox'>
+                          <input type='checkbox' value='Checkbox 1' id='form-checkbox-1' name='form-checkbox' />
+                          <span className='form__option__text'>{o}</span>
+                          <span className='form__option__ui'></span>
+                        </label>
+                      );
+                    })}
+                  </ScrollArea>
+                </div>
+              </div>
+            </aside>
+
+            <div className='inpage__content'>
+              <div className='content__meta'>
+                <div className='filters-summary'>
+                  <h3 className='filters-summary-title'>Filters <a href='' title='Clear all selected filters'><small>(Clear All)</small></a></h3>
+                  <button type='button' className='button--filter-pill'><span>Australia</span></button>
+                  <button type='button' className='button--filter-pill'><span>PM2.5</span></button>
+                  <button type='button' className='button--filter-pill'><span>O3</span></button>
+                </div>
+
+                <div className='content__actions'>
+                  <h3>Sort By</h3>
+                  <Dropdown
+                    triggerElement='a'
+                    triggerClassName='button button--primary-bounded drop__toggle--caret'
+                    triggerTitle='Show/hide sort options'
+                    triggerText='Recently Updated' >
+
+                    <ul role='menu' className='drop__menu drop__menu--select'>
+                      <li><a className='drop__menu-item drop__menu-item--active' title='This is Item 1b' href='#'>Recenty Updated</a></li>
+                      <li><a className='drop__menu-item' title='This is Item 2b' href='#'>Name</a></li>
+                      <li><a className='drop__menu-item' title='This is Item 3b' href='#'>Measurements</a></li>
+                    </ul>
+                  </Dropdown>
+                </div>
+
+                <div className='content__heading'>
+                  <h2 className='content-prime-title'>Results <small>Showing <strong>1235</strong> locations</small></h2>
+                </div>
+              </div>
+
+              <div className='inpage__results'>
+                <LocationCard />
+                <LocationCard />
+                <LocationCard />
+                <LocationCard />
+                <LocationCard />
+              </div>
+
+              <ReactPaginate
+                previousLabel={<span>previous</span>}
+                nextLabel={<span>next</span>}
+                breakLabel={<span className='pages__page'>...</span>}
+                pageNum={15}
+                forceSelected={3 - 1}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                clickCallback={this.handlePageClick}
+                containerClassName={'pagination'}
+                subContainerClassName={'pages'}
+                pageClassName={'pages__wrapper'}
+                pageLinkClassName={'pages__page'}
+                activeClassName={'active'} />
+
+              <div className='disclaimers'>
+                <p>Data are from official, stationary government monitors and not smaller-scale or mobile monitors. It is our intent to attribute all data to their originating sources. Please contact us if you notice otherwise. </p>
+                <p>Note: We do not guarantee the accuracy of any data aggregated to the platform.  Please see originating sites for more information.</p>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     );
