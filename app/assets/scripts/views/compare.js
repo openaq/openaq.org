2028cd44d53bea54032f87cbc7d044ada7758d87
Daniel da Silva <daniel@developmentseed.org>
2016-08-09 17:38:36 +0100

    Add location header to compare page

--- app/assets/scripts/views/compare.js
+++ app/assets/scripts/views/compare.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import ShareBtn from '../components/share-btn';
+
 var Compare = React.createClass({
   displayName: 'Compare',
 
@@ -9,17 +11,51 @@ var Compare = React.createClass({
   },
 
   render: function () {
-    console.log('thi', this.props.params);
     return (
       <section className='inpage'>
         <header className='inpage__header'>
           <div className='inner'>
             <div className='inpage__headline'>
               <h1 className='inpage__title'>Compare page</h1>
-              <div className='inpage__introduction'>
-                <p>Good things come to those who wait...</p>
+              <div className='inpage__headline-actions'>
+                <ShareBtn />
               </div>
             </div>
+
+            <div className='compare'>
+              <ul className='compare__location-list'>
+                <li className='compare__location'>
+                  <p className='compare__subtitle'>Updates hourly</p>
+                  <h2 className='compare__title'><span className='compare-marker compare-marker--st'>Location</span> <small>in area, country</small></h2>
+                  <div className='compare__actions'>
+                    <button type='button' className='button button--small button--primary-unbounded'>Edit</button>
+                    <button type='button' className='button button--small button--primary-unbounded'>Delete</button>
+                  </div>
+                </li>
+                <li className='compare__location'>
+                  <p className='compare__subtitle'>Updates hourly</p>
+                  <h2 className='compare__title'><span className='compare-marker compare-marker--nd'>Location</span> <small>in area, country</small></h2>
+                  <div className='compare__actions'>
+                    <button type='button' className='button button--small button--primary-unbounded'>Edit</button>
+                    <button type='button' className='button button--small button--primary-unbounded'>Delete</button>
+                  </div>
+                </li>
+                {/*
+                <li className='compare__location'>
+                  <p className='compare__subtitle'>Updates hourly</p>
+                  <h2 className='compare__title'><span className='compare-marker compare-marker--rd'>Location</span> <small>in area, country</small></h2>
+                  <div className='compare__actions'>
+                    <button type='button' className='button button--small button--primary-unbounded'>Edit</button>
+                    <button type='button' className='button button--small button--primary-unbounded'>Delete</button>
+                  </div>
+                </li>
+                */}
+                <li className='compare__location-actions'>
+                  <button type='button' className='button-compare-location'>Add Location</button>
+                </li>
+              </ul>
+            </div>
+
           </div>
         </header>
         <div className='inpage__body'>
@@ -30,7 +66,6 @@ var Compare = React.createClass({
                 <h1 className='fold__title'>Our data</h1>
               </header>
               <div className='fold__body'>
-                let's compare some locations
               </div>
             </div>
           </section>
