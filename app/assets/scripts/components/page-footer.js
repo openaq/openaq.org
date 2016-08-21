42c600a08386e1fb6f382d7d9c5bbe7f5fc816c8
Daniel da Silva <daniel@developmentseed.org>
2016-08-21 10:53:08 +0100

    Connect mailchimp newsletter form

--- app/assets/scripts/components/page-footer.js
+++ app/assets/scripts/components/page-footer.js
@@ -28,10 +28,16 @@ var PageFooter = React.createClass({
             </div>
             <div className='contact contact--newsletter'>
               <h2 className='contact__title'>Subscribe to our newsletter</h2>
-              <div className='form__input-group'>
-                <input type='text' placeholder='bruce@wayne.com' className='form__control form__control--medium' />
-                <span className='form__input-group-button'><button className='button--subscribe' type='submit'><span>Subscribe</span></button></span>
-              </div>
+              <form ref='newsletterForm' action='//openaq.us10.list-manage.com/subscribe/post?u=ca93b2911fff40db15f6e7203&amp;id=e65a8618a1' method='post' id='mc-embedded-subscribe-form' name='mc-embedded-subscribe-form' target='_blank' noValidate>
+                <div className='form__input-group'>
+                  <input type='email' name='EMAIL' id='mce-EMAIL' placeholder='bruce@wayne.com' className='form__control form__control--medium' required/>
+                  <span className='form__input-group-button'><button className='button--subscribe' type='submit' onClick={() => this.refs.newsletterForm.reset()}><span>Subscribe</span></button></span>
+                </div>
+                {/* real people should not fill this in and expect good things - do not remove this or risk form bot signups */}
+                <div style={{position: 'absolute', left: '-5000px'}} aria-hidden='true'>
+                  <input type='text' name='b_ca93b2911fff40db15f6e7203_e65a8618a1' tabIndex='-1' value='' />
+                </div>
+              </form>
             </div>
           </div>
           <p className='copyright'>
