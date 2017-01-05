1bff01c8bbb1cbf761fa29d75b3bd4f5c7f7a962
Andrew Carlson <contact@andrewcarlsonweb.com>
2017-01-05 07:53:19 -0600

    Replaces the testing URL and also fixes ticket 168

--- app/assets/scripts/components/share-btn.js
+++ app/assets/scripts/components/share-btn.js
@@ -10,7 +10,7 @@ var ShareBtn = React.createClass({
 
   render: function () {
 
-    var encodedURl = encodeURIComponent("https://openaq.org/#/compare/JOHN/40GK09%20-%20GENK?parameter=pm25&_k=27lnfl");
+    var encodedURl = encodeURIComponent(window.location.href);
 
     return (
       <Dropdown
@@ -20,7 +20,7 @@ var ShareBtn = React.createClass({
         triggerText='Share' >
 
         <ul role='menu' className='drop__menu drop__menu--select'>
-          <li><a className='drop__menu-item share-opt-twitter' href={`https://twitter.com/share?url=${window.location.url}`} target='_blank' title='Share on twitter' data-hook='dropdown:close'><span>Twitter</span></a></li>
+          <li><a className='drop__menu-item share-opt-twitter' href={`https://twitter.com/share?url=${encodedURl}`} target='_blank' title='Share on twitter' data-hook='dropdown:close'><span>Twitter</span></a></li>
           <li><a className='drop__menu-item share-opt-facebook' href={`http://facebook.com/sharer.php?u=${encodedURl}`} target='_blank' title='Share on Facebook' data-hook='dropdown:close'><span>Facebook</span></a></li>
         </ul>
       </Dropdown>
