6f69f1c945580af69b6de6a7016a6626118bdda8
Ricardo Mestre <ricardo@duplos.org>
2019-07-23 15:48:38 +0100

    Structure and style home join fold

--- app/assets/scripts/views/home.js
+++ app/assets/scripts/views/home.js
@@ -291,6 +291,46 @@ var Home = React.createClass({
             </div>
           </section>
 
+          <section className='join-fold'>
+            <div className='inner'>
+              <header className='join-fold__header'>
+                <h1 className='join-fold__title'>Join a community that believes everyone should have access to clean air.</h1>
+              </header>
+
+              <div className='join-fold__cards'>
+                <article className='join-card'>
+                  <div className='inner'>
+                    <div className='join-card__media'>
+                      <figure>
+                        <img src='/assets/graphics/layout/oaq-icon-illu-40-join-community.svg' width='40' height='40' alt='Illustration' />
+                      </figure>
+                    </div>
+                    <div className='join-card__copy'>
+                      <h1 className='join-card__title'>Participate</h1>
+                      <p>Be a data source, a consumer or a platform builder.</p>
+                      <p className='join-card__action'><a href='#' className='join-card__go-link' title='View more'>Participate<span></span></a></p>
+                    </div>
+                  </div>
+                </article>
+
+                <article className='join-card'>
+                  <div className='inner'>
+                    <div className='join-card__media'>
+                      <figure>
+                        <img src='/assets/graphics/layout/oaq-icon-illu-40-support.svg' width='40' height='40' alt='Illustration' />
+                      </figure>
+                    </div>
+                    <div className='join-card__copy'>
+                      <h1 className='join-card__title'>Support our mission</h1>
+                      <p>Be a financial partner to help fight air inequality.</p>
+                      <p className='join-card__action'><a href='#' className='join-card__go-link' title='View more'>Support<span></span></a></p>
+                    </div>
+                  </div>
+                </article>
+              </div>
+            </div>
+          </section>
+
         </div>
       </section>
     );
