bcba4e35bf9e0c1861b8470b2783f3084b6c3733
Ricardo Mestre <ricardo@duplos.org>
2019-07-25 12:00:46 +0100

    Include home cover

--- app/assets/scripts/views/home.js
+++ app/assets/scripts/views/home.js
@@ -186,6 +186,11 @@ var Home = React.createClass({
               </article>
             </div>
           </div>
+          <figure className='inpage__media inpage__media--cover media'>
+            <div className='media__item'>
+              <img src='/assets/graphics/content/view--home/cover--home.jpg' alt='Cover image' width='1440' height='712' />
+            </div>
+          </figure>
         </header>
         <div className='inpage__body'>
 
