4f610c25bb30f29611ec833cb7b935efd01ecd67
Ricardo Mestre <ricardo@duplos.org>
2019-08-01 16:52:54 +0100

    Structure and style about book fold

--- app/assets/scripts/views/about.js
+++ app/assets/scripts/views/about.js
@@ -112,29 +112,63 @@ var About = React.createClass({
                 <h1 className='fold__title'>Partners and Sponsors</h1>
               </header>
               <ul className='sponsor-list'>
-                <li><a className='sponsor-list__item' target='_blank' href='https://www.climateworks.org/'><img src='assets/graphics/content/sponsors/climateworks.jpg' alt='View sponsor website'/></a></li>
-                <li><a className='sponsor-list__item' target='_blank' href='https://developmentseed.org/'><img src='assets/graphics/content/sponsors/devseed.png' alt='View sponsor website'/></a></li>
-                <li><a className='sponsor-list__item' target='_blank' href='https://aws.amazon.com/'><img src='assets/graphics/content/sponsors/aws.png' alt='View sponsor website'/></a></li>
-                <li><a className='sponsor-list__item' target='_blank' href='http://www.fz-juelich.de/portal/DE/Home/home_node.html/'><img src='assets/graphics/content/sponsors/fj.png' alt='View sponsor website'/></a></li>
-                <li><a className='sponsor-list__item' target='_blank' href='https://erc.europa.eu/'><img src='assets/graphics/content/sponsors/erc.png' alt='View sponsor website'/></a></li>
-                <li><a className='sponsor-list__item' target='_blank' href='http://sites.agu.org/'><img src='assets/graphics/content/sponsors/agu.jpg' alt='View sponsor website'/></a></li>
-                <li><a className='sponsor-list__item' target='_blank' href='http://earthjournalism.net/'><img src='assets/graphics/content/sponsors/ejn.png' alt='View sponsor website'/></a></li>
-                <li><a className='sponsor-list__item' target='_blank' href='https://www.internews.org/'><img src='assets/graphics/content/sponsors/internews.png' alt='View sponsor website'/></a></li>
-                <li><a className='sponsor-list__item' target='_blank' href='https://keen.io/'><img src='assets/graphics/content/sponsors/keenio.jpg' alt='View sponsor website'/></a></li>
-                <li><a className='sponsor-list__item' target='_blank' href='https://www.nih.gov/'><img src='assets/graphics/content/sponsors/nih.jpg' alt='View sponsor website'/></a></li>
-                <li><a className='sponsor-list__item' target='_blank' href='https://www.openscienceprize.org/'><img src='assets/graphics/content/sponsors/openscience.png' alt='View sponsor website'/></a></li>
-                <li><a className='sponsor-list__item' target='_blank' href='http://thrivingearthexchange.org/'><img src='assets/graphics/content/sponsors/tex.png' alt='View sponsor website'/></a></li>
-                <li><a className='sponsor-list__item' target='_blank' href='https://wellcome.ac.uk/'><img src='assets/graphics/content/sponsors/wellcome.jpg' alt='View sponsor website'/></a></li>
-                <li><a className='sponsor-list__item' target='_blank' href='https://www.hhmi.org/'><img src='assets/graphics/content/sponsors/hhmi.jpg' alt='View sponsor website'/></a></li>
-                <li><a className='sponsor-list__item' target='_blank' href='http://www.echoinggreen.org/'><img src='assets/graphics/content/sponsors/echoing-green.jpeg' alt='View sponsor website'/></a></li>
-                <li><a className='sponsor-list__item' target='_blank' href='http://www.wri.org/'><img src='assets/graphics/content/sponsors/WRI.jpg' alt='View sponsor website'/></a></li>
-                <li><a className='sponsor-list__item' target='_blank' href='https://www.paulhastings.com/'><img src='assets/graphics/content/sponsors/PH.jpg' alt='View sponsor website'/></a></li>
-                <li><a className='sponsor-list__item' target='_blank' href='http://godleyfamilyfoundation.org/'><img src='assets/graphics/content/sponsors/godley.png' alt='View sponsor website'/></a></li>
-                <li><a className='sponsor-list__item' target='_blank' href='https://www.radiant.earth/'><img src='assets/graphics/content/sponsors/radiantearth.png' alt='View sponsor website'/></a></li>
+                <li><a title='Visit sponsor website' className='sponsor-list__item' target='_blank' href='https://www.climateworks.org/'><img src='/assets/graphics/content/sponsors/climateworks.jpg' alt='Sponsor logo' /></a></li>
+                <li><a title='Visit sponsor website' className='sponsor-list__item' target='_blank' href='https://developmentseed.org/'><img src='/assets/graphics/content/sponsors/devseed.png' alt='Sponsor logo' /></a></li>
+                <li><a title='Visit sponsor website' className='sponsor-list__item' target='_blank' href='https://aws.amazon.com/'><img src='/assets/graphics/content/sponsors/aws.png' alt='Sponsor logo' /></a></li>
+                <li><a title='Visit sponsor website' className='sponsor-list__item' target='_blank' href='http://www.fz-juelich.de/portal/DE/Home/home_node.html/'><img src='/assets/graphics/content/sponsors/fj.png' alt='Sponsor logo' /></a></li>
+                <li><a title='Visit sponsor website' className='sponsor-list__item' target='_blank' href='https://erc.europa.eu/'><img src='/assets/graphics/content/sponsors/erc.png' alt='Sponsor logo' /></a></li>
+                <li><a title='Visit sponsor website' className='sponsor-list__item' target='_blank' href='http://sites.agu.org/'><img src='/assets/graphics/content/sponsors/agu.jpg' alt='Sponsor logo' /></a></li>
+                <li><a title='Visit sponsor website' className='sponsor-list__item' target='_blank' href='http://earthjournalism.net/'><img src='/assets/graphics/content/sponsors/ejn.png' alt='Sponsor logo' /></a></li>
+                <li><a title='Visit sponsor website' className='sponsor-list__item' target='_blank' href='https://www.internews.org/'><img src='/assets/graphics/content/sponsors/internews.png' alt='Sponsor logo' /></a></li>
+                <li><a title='Visit sponsor website' className='sponsor-list__item' target='_blank' href='https://keen.io/'><img src='/assets/graphics/content/sponsors/keenio.jpg' alt='Sponsor logo' /></a></li>
+                <li><a title='Visit sponsor website' className='sponsor-list__item' target='_blank' href='https://www.nih.gov/'><img src='/assets/graphics/content/sponsors/nih.jpg' alt='Sponsor logo' /></a></li>
+                <li><a title='Visit sponsor website' className='sponsor-list__item' target='_blank' href='https://www.openscienceprize.org/'><img src='/assets/graphics/content/sponsors/openscience.png' alt='Sponsor logo' /></a></li>
+                <li><a title='Visit sponsor website' className='sponsor-list__item' target='_blank' href='http://thrivingearthexchange.org/'><img src='/assets/graphics/content/sponsors/tex.png' alt='Sponsor logo' /></a></li>
+                <li><a title='Visit sponsor website' className='sponsor-list__item' target='_blank' href='https://wellcome.ac.uk/'><img src='/assets/graphics/content/sponsors/wellcome.jpg' alt='Sponsor logo' /></a></li>
+                <li><a title='Visit sponsor website' className='sponsor-list__item' target='_blank' href='https://www.hhmi.org/'><img src='/assets/graphics/content/sponsors/hhmi.jpg' alt='Sponsor logo' /></a></li>
+                <li><a title='Visit sponsor website' className='sponsor-list__item' target='_blank' href='http://www.echoinggreen.org/'><img src='/assets/graphics/content/sponsors/echoing-green.jpeg' alt='Sponsor logo' /></a></li>
+                <li><a title='Visit sponsor website' className='sponsor-list__item' target='_blank' href='http://www.wri.org/'><img src='/assets/graphics/content/sponsors/WRI.jpg' alt='Sponsor logo' /></a></li>
+                <li><a title='Visit sponsor website' className='sponsor-list__item' target='_blank' href='https://www.paulhastings.com/'><img src='/assets/graphics/content/sponsors/PH.jpg' alt='Sponsor logo' /></a></li>
+                <li><a title='Visit sponsor website' className='sponsor-list__item' target='_blank' href='http://godleyfamilyfoundation.org/'><img src='/assets/graphics/content/sponsors/godley.png' alt='Sponsor logo' /></a></li>
+                <li><a title='Visit sponsor website' className='sponsor-list__item' target='_blank' href='https://www.radiant.earth/'><img src='/assets/graphics/content/sponsors/radiantearth.png' alt='Sponsor logo' /></a></li>
               </ul>
               <footer className='fold__footer'>
                 <a href='mailto:' className='sponsor-button' title='View page'><span>Become a sponsor</span></a>
               </footer>
+            </div>
+          </section>
+
+          <section className='fold fold--type-b' id='about-fold-book'>
+            <div className='inner'>
+              <header className='fold__header'>
+                <h1 className='fold__title'>We are an open book</h1>
+                <div className='fold__teaser prose prose--responsive'>
+                  <p>We value transparency so all our records are publically available.</p>
+                  <p className='fold__main-action'>
+                    <a href='#' className='button-book-download' title='Download'><span>Download 2018 Form 990</span></a>
+                  </p>
+                </div>
+              </header>
+              <figure className='fold__media'>
+                <article className='card card--book'>
+                  <a href='#' className='card__contents' title='Download'>
+                    <header className='card__header'>
+                      <div className='card__headline'>
+                        <p className='card__subtitle'>2018</p>
+                        <h1 className='card__title'>Form 990</h1>
+                      </div>
+                    </header>
+                    <figure className='card__media'>
+                      <div className='card__thumb'>
+                        <img src='/assets/graphics/content/view--about/card-book-media.jpg' width='724' height='348' alt='Card media' />
+                      </div>
+                    </figure>
+                    <footer className='card__footer'>
+                      <img src='/assets/graphics/layout/oaq-logo-col-pos.svg' alt='OpenAQ logotype' width='72' height='40' />
+                    </footer>
+                  </a>
+                </article>
+              </figure>
             </div>
           </section>
 
