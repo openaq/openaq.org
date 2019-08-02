e4a34682267bac99b22624ff5d60d4de52cb2705
Olaf Veerman <olaf.veerman@gmail.com>
2019-08-02 06:48:37 -0400

    Fix linting errors

--- app/assets/scripts/components/community-card.js
+++ app/assets/scripts/components/community-card.js
@@ -1,6 +1,5 @@
 'use strict';
 import React from 'react';
-import c from 'classnames';
 
 var CommunityCard = React.createClass({
   displayName: 'CommunityCard',
--- app/assets/scripts/components/connect-fold.js
+++ app/assets/scripts/components/connect-fold.js
@@ -1,6 +1,5 @@
 'use strict';
 import React from 'react';
-import { Link } from 'react-router';
 
 var ConnectFold = React.createClass({
   displayName: 'ConnectFold',
