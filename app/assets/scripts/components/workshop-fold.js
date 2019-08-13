4daa3112eafe3982c175ddd16ff4c3e1cb182c53
Ricardo Mestre <ricardo@duplos.org>
2019-08-13 15:42:25 +0100

    Add placeholder link to workshop fold

--- app/assets/scripts/components/workshop-fold.js
+++ app/assets/scripts/components/workshop-fold.js
@@ -1,5 +1,6 @@
 'use strict';
 import React from 'react';
+import { Link } from 'react-router';
 
 var WorkshopFold = React.createClass({
   displayName: 'WorkshopFold',
@@ -12,7 +13,7 @@ var WorkshopFold = React.createClass({
             <h1 className='workshop-fold__title'>Hold a Workshop</h1>
             <div className='prose prose--responsive'>
               <p>Interested in convening various members of your community around open air quality data? We can help!</p>
-              <p><a href='#' className='workshop-go-button' title='Learn more'><span>Learn how</span></a></p>
+              <p><Link to='/community' className='workshop-go-button' title='Learn more'><span>Learn how</span></Link></p>
             </div>
           </header>
 
