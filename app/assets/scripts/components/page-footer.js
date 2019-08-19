1ef6ca7a2d0eeaf5992fdf7e6ad27c69b5f4722c
Olaf Veerman <olaf.veerman@gmail.com>
2019-08-19 15:18:37 -0400

    Remove DevSeed from footer

--- app/assets/scripts/components/page-footer.js
+++ app/assets/scripts/components/page-footer.js
@@ -89,7 +89,7 @@ var PageFooter = React.createClass({
               </form>
             </div>
 
-            <p className='foot-copyright'>{copyright} with love by <a href='https://developmentseed.org' title='Visit Development Seed website'>Development Seed</a> and the <a href='https://openaq.org/' title='Visit the OpenAQ website'>OpenAQ</a> team.</p>
+            <p className='foot-copyright'>{copyright} with love by <a href='https://openaq.org/' title='Visit the OpenAQ website'>OpenAQ</a>.</p>
           </div>
         </div>
       </footer>
