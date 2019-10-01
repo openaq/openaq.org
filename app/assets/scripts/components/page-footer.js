b55c7fc9062751a4acb8b44c7534043daebdd708
Donald McKendrick <ddmckendrick@gmail.com>
2019-10-01 13:27:02 -0700

    Update twitter link in page footer

--- app/assets/scripts/components/page-footer.js
+++ app/assets/scripts/components/page-footer.js
@@ -29,7 +29,7 @@ var PageFooter = React.createClass({
               <ul className='connect-menu'>
                 <li><a href='https://github.com/openaq/' className='connect-menu__link--github' title='View Github' target='_blank'><span>Github</span></a></li>
                 <li><a href='https://openaq-slackin.herokuapp.com/' className='connect-menu__link--slack' title='View Slack' target='_blank'><span>Slack</span></a></li>
-                <li><a href='https://twitter.com/open_aq' className='connect-menu__link--twitter' title='View Twitter' target='_blank'><span>Twitter</span></a></li>
+                <li><a href='https://twitter.com/openaq' className='connect-menu__link--twitter' title='View Twitter' target='_blank'><span>Twitter</span></a></li>
                 <li><a href='mailto:info@openaq.org' className='connect-menu__link--email' title='View Email'><span>Email</span></a></li>
               </ul>
             </div>
