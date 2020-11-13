a6f3bf2305d6f4391b0ece79c3c98e12b4aba7a7
Alice Rühl <alice@developmentseed.org>
2020-11-13 16:50:51 +0100

    Format merged files

--- app/assets/scripts/components/card-list.js
+++ app/assets/scripts/components/card-list.js
@@ -4,25 +4,21 @@ import styled, { css } from 'styled-components';
 
 const Wrapper = styled.div`
   display: grid;
-  ${({gridGap, gridTemplateRows, gridTemplateColumns}) => css`
+  ${({ gridGap, gridTemplateRows, gridTemplateColumns }) => css`
     grid-template-columns: ${gridTemplateColumns || 'repeat(4, 1fr)'};
     grid-template-rows: ${gridTemplateRows || 'auto'};
     grid-gap: ${gridGap || '2rem'};
   `}
 `;
 
-function CardList (props) {
-  const {
-    gridGap,
-    gridTemplateColumns,
-    gridTemplateRows
-  } = props;
+function CardList(props) {
+  const { gridGap, gridTemplateColumns, gridTemplateRows } = props;
   return (
     <Wrapper
       {...{
         gridGap,
         gridTemplateColumns,
-        gridTemplateRows
+        gridTemplateRows,
       }}
     >
       {props.children}
@@ -34,7 +30,7 @@ CardList.propTypes = {
   gridGap: T.string,
   gridTemplateRows: T.string,
   gridTemplateColumns: T.string,
-  children: T.node
+  children: T.node,
 };
 
 export default CardList;
