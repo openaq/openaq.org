77dd3a4daa8a616a2a927b0c2a25f4b0b71dd602
Jeevan Farias <jeevan@developmentseed.org>
2020-11-16 10:56:44 -0500

    Add table

---
+++ app/assets/scripts/components/table.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import T from 'prop-types';
+
+
+function Table (props) {
+  const {
+  return (
+  );
+}
+
+Table.propTypes = {
+};
+
+export default Table;
