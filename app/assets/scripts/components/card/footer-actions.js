3dea77d18bdac0d5fc2404f8a7613474e4e485bb
Alice Rühl <alice@developmentseed.org>
2020-11-23 15:21:31 +0100

    Disable non-functional link

--- app/assets/scripts/components/card/footer-actions.js
+++ app/assets/scripts/components/card/footer-actions.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { PropTypes as T } from 'prop-types';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const DisabledLink = styled(Link)`
+  pointer-events: none;
+  color: grey;
+  &:visited {
+    color: grey;
+  }
+`;
 
 export default function FooterActions({ what, onDownloadClick, viewMorePath }) {
   return (
@@ -16,9 +25,14 @@ export default function FooterActions({ what, onDownloadClick, viewMorePath }) {
         </a>
       </li>
       <li>
-        <Link to={viewMorePath} className="cfa-go" title={`View ${what} page`}>
+        <DisabledLink
+          disabled
+          to={viewMorePath}
+          className="cfa-go"
+          title={`View ${what} page`}
+        >
           View More
-        </Link>
+        </DisabledLink>
       </li>
     </ul>
   );
