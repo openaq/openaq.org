492547c670be79cebdc1e334bfcab13e19a78dfa
Jeevan Farias <jeevan@developmentseed.org>
2020-12-01 11:15:13 -0500

    Fully implement card refactor

---
+++ app/assets/scripts/views/locations-hub/location-card.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import { PropTypes as T } from 'prop-types';
+import moment from 'moment';
+
+import { formatThousands } from '../../utils/format';
+import Card, { CardDetails, FooterActions } from '../../components/card';
+
+export default function LocationCard({
+  onDownloadClick,
+  lastUpdated,
+  name,
+  subtitle,
+  sourceType,
+  firstUpdated,
+  totalLocations,
+  totalMeasurements,
+  parametersList,
+  id,
+  city,
+  countryData,
+  sourcesData,
+  lastUpdate,
+  collectionStart,
+}) {
+  let updated = moment(lastUpdated).fromNow();
+  let started = moment(firstUpdated).format('YYYY/MM/DD');
+  let ended = moment(lastUpdated).format('YYYY/MM/DD');
+
+  const country = countryData || {};
+  let sources = [];
+  if (sourcesData.length) {
+    sourcesData.forEach((o, i) => {
+      sources.push(
+        <a href={o.sourceURL} title={`View source for  ${name}`} key={o.name}>
+          {o.name}
+        </a>
+      );
+      if (i < sourcesData.length - 1) {
+        sources.push(', ');
+      }
+    });
+  }
+
+  return (
+    <Card
+      title={
+        <>
+          {name} <small>{subtitle}</small>
+          <small>
+            in {city}, {country.name}
+          </small>
+        </>
+      }
+      subtitle={
+        <>
+          Updated <strong>{updated}</strong>
+        </>
+      }
+      tags={sourceType}
+      renderBody={() => (
+        <CardDetails
+          list={[
+            { label: 'Collection started', value: started },
+            {
+              label: 'Measurements',
+              value: formatThousands(totalMeasurements),
+            },
+            { label: 'Values', value: parametersList.map(p => p.name).join(', ') },
+            ...(sources.length ? [{ label: 'Sources', value: sources }] : []),
+          ]}
+        />
+      )}
+      renderFooter={() => (
+        <FooterActions
+          what={name}
+          onDownloadClick={onDownloadClick}
+          viewMorePath={`/location/${encodeURIComponent(id)}`}
+        />
+      )}
+    />
+  );
+}
+
+LocationCard.propTypes = {
+  onDownloadClick: T.func,
+  lastUpdated: T.string,
+  name: T.string,
+  subtitle: T.string,
+  sourceType: T.oneOfType([T.array, T.string]),
+  firstUpdated: T.string,
+  totalLocations: T.number,
+  totalMeasurements: T.number,
+  parametersList: T.array,
+
+  compact: T.bool,
+  id: T.string,
+  city: T.string,
+  countryData: T.object,
+  sourcesData: T.array,
+  lastUpdate: T.string,
+  collectionStart: T.string,
+};
---
+++ app/assets/scripts/views/locations-hub/results.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import { PropTypes as T } from 'prop-types';
+import ReactPaginate from 'react-paginate';
+import _ from 'lodash';
+
+import InfoMessage from '../../components/info-message';
+import LoadingMessage from '../../components/loading-message';
+import LocationCard from './location-card';
+
+export default function Results({
+  fetched,
+  fetching,
+  error,
+  locations,
+  countries,
+  sources,
+  parameters,
+  totalPages,
+  page,
+  openDownloadModal,
+  handlePageClick,
+}) {
+  if (!fetched && !fetching) {
+    return null;
+  }
+
+  if (fetching) {
+    return <LoadingMessage />;
+  }
+
+  if (error) {
+    return (
+      <InfoMessage>
+        <p>We coudn&apos;t get the data. Please try again later.</p>
+        <p>
+          If you think there&apos;s a problem, please{' '}
+          <a href="mailto:info@openaq.org" title="Contact openaq">
+            contact us.
+          </a>
+        </p>
+      </InfoMessage>
+    );
+  }
+
+  if (!locations.length) {
+    return (
+      <InfoMessage>
+        <p>No data was found for your criteria.</p>
+        <p>
+          Maybe you&apos;d like to suggest a{' '}
+          <a
+            href="https://docs.google.com/forms/d/1Osi0hQN1-2aq8VGrAR337eYvwLCO5VhCa3nC_IK2_No/viewform"
+            title="Suggest a new source"
+          >
+            new source
+          </a>{' '}
+          or{' '}
+          <a href="mailto:info@openaq.org" title="Contact openaq">
+            let us know
+          </a>{' '}
+          what location you&apos;d like to see data for.
+        </p>
+      </InfoMessage>
+    );
+  }
+
+  return (
+    <>
+      <div className="inpage__results">
+        {locations.map(loc => {
+          let countryData = _.find(countries, { code: loc.country });
+          let sourcesData = loc.sourceNames
+            .map(s => _.find(sources, { name: s }))
+            .filter(s => s);
+          let params = loc.parameters.map(o => _.find(parameters, { id: o }));
+
+          let openModal = () =>
+            openDownloadModal({
+              country: loc.country,
+              area: loc.city,
+              location: loc.location,
+            });
+          return (
+            <LocationCard
+              id={loc.id}
+              onDownloadClick={openModal}
+              key={loc.location}
+              name={loc.location}
+              city={loc.city}
+              sourceType={loc.sourceType}
+              countryData={countryData}
+              sourcesData={sourcesData}
+              totalMeasurements={loc.count}
+              parametersList={params}
+              lastUpdate={loc.lastUpdated}
+              collectionStart={loc.firstUpdated}
+            />
+          );
+        })}
+      </div>
+      {!(!fetched || error || !locations.length) && (
+        <ReactPaginate
+          previousLabel={<span>previous</span>}
+          nextLabel={<span>next</span>}
+          breakLabel={<span className="pages__page">...</span>}
+          pageCount={totalPages}
+          forcePage={page - 1}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={4}
+          onPageChange={handlePageClick}
+          containerClassName={'pagination'}
+          subContainerClassName={'pages'}
+          pageClassName={'pages__wrapper'}
+          pageLinkClassName={'pages__page'}
+          activeClassName={'active'}
+        />
+      )}
+    </>
+  );
+}
+
+Results.propTypes = {
+  fetched: T.bool,
+  fetching: T.bool,
+  error: T.bool,
+  projects: T.array,
+  totalPages: T.number,
+  page: T.number,
+  openDownloadModal: T.func,
+  handlePageClick: T.func,
+  history: T.object,
+};
