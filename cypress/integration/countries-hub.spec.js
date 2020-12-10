f8f9f26f3fa25e8a70a744376aca5a0d21fc4759
Alice Rühl <alice@developmentseed.org>
2020-12-10 16:12:32 +0100

    Increase test coverage

--- cypress/integration/countries-hub.spec.js
+++ cypress/integration/countries-hub.spec.js
@@ -1,12 +1,26 @@
 // / <reference types="Cypress" />
 
 describe('The Countries Hub', () => {
-  it('successfully loads', () => {
+  before(() => {
     cy.visit('/#/countries');
+  });
 
+  it('successfully loads', () => {
     cy.get('header');
     cy.get('main');
     cy.get('footer');
     cy.get('h1');
+  });
+
+  it('has a header with title and description', () => {
+    cy.get('h1').should('contain', 'Browse by Country');
+    cy.get('.inpage__introduction').should(
+      'contain',
+      'We are currently collecting data in 82 different countries and are always seeking to add more. We aggregate PM2.5, PM10, ozone (O3), sulfur dioxide (SO2), nitrogen dioxide (NO2), carbon monoxide (CO), and black carbon (BC) from real-time government and research grade sources. If you cannot find the location that you are looking for, please '
+    );
+  });
+
+  it('has a results section with a list of country cards', () => {
+    cy.get('.card').should('have.length', 82);
   });
 });
--- cypress/integration/locations-hub.spec.js
+++ cypress/integration/locations-hub.spec.js
@@ -1,12 +1,43 @@
 // / <reference types="Cypress" />
 
 describe('The Locations Hub', () => {
-  it('successfully loads', () => {
+  before(() => {
     cy.visit('/#/locations');
+  });
 
+  it('successfully loads', () => {
     cy.get('header');
     cy.get('main');
     cy.get('footer');
     cy.get('h1');
+  });
+
+  it('has a header with title and description', () => {
+    cy.get('h1').should('contain', 'Locations');
+    cy.get('.inpage__introduction').should(
+      'contain',
+      'Lorem ipsum dolor sit amet'
+    );
+  });
+
+  it('has a results section with a list of location cards', () => {
+    cy.get('.results-summary').should(
+      'have.text',
+      'A total of 12005 locations were found'
+    );
+
+    cy.get('.card').should('have.length', 15);
+    cy.get('.pagination').should('exist');
+  });
+
+  it('has some filters with dropdown menus', () => {
+    cy.get('.filters').should('exist');
+
+    cy.get('[title="type__filter"]').click();
+    cy.get('.drop__menu-item').first().click();
+
+    cy.get('.button--filter-pill').should('exist');
+
+    cy.get('button').contains('Clear Filters').should('exist');
   });
 });
