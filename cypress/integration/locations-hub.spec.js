73feba5f1ab6fc1c9588a08df6f7c24f67fd8087
Jeevan Farias <jeevan@developmentseed.org>
2020-12-15 09:48:04 -0500

    Temp fix, increase timeout for failing test

--- cypress/integration/locations-hub.spec.js
+++ cypress/integration/locations-hub.spec.js
@@ -34,7 +34,7 @@ describe('The Locations Hub', () => {
 
     cy.get('[title="type__filter"]').click();
     cy.get('.drop__menu-item').first().click();
-
+    cy.wait(1000)
     cy.get('.button--filter-pill').should('exist');
     cy.get('button').contains('Clear Filters').should('exist');
 
