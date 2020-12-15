3e0a4baf9818053998411c0739bfccedc3d2495c
Alice Rühl <alice@developmentseed.org>
2020-12-15 20:52:14 +0100

    Fix lint
    Not sure if the test works with that solution on ci, locally it does

--- cypress/integration/locations-hub.spec.js
+++ cypress/integration/locations-hub.spec.js
@@ -34,8 +34,8 @@ describe('The Locations Hub', () => {
 
     cy.get('[title="type__filter"]').click();
     cy.get('.drop__menu-item').first().click();
-    cy.wait(1000)
-    cy.get('.button--filter-pill').should('exist');
+
+    cy.get('.button--filter-pill').should('exist').and('be.visible');
     cy.get('button').contains('Clear Filters').should('exist');
 
     cy.get('.button--filter-pill').click();
