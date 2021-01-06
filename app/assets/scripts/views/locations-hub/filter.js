a692fc3a001e8446b36264f5f480aa54255e27ed
Jeevan Farias <jeevan@developmentseed.org>
2021-01-06 16:22:22 -0500

    Use param display name on locations hub

--- app/assets/scripts/views/locations-hub/filter.js
+++ app/assets/scripts/views/locations-hub/filter.js
@@ -48,7 +48,7 @@ export default function Filter({ countries, parameters, sources }) {
   sortList(countries);
   sortList(parameters);
 
-  const parameterNames = [...new Set(parameters.map(p => p.name))];
+  //const parameterNames = [...new Set(parameters.map(p => p.name))];
 
   function onFilterSelect(what, value) {
     let query = qs.parse(location.search, {
@@ -190,20 +190,20 @@ export default function Filter({ countries, parameters, sources }) {
           >
             {/* references list of unique ids to avoid duplicate list items while allowing 
             selection of parameters with different units and shared id */}
-            {_.sortBy(parameterNames).map(paramName => {
+            {_.sortBy(parameters).map(param => {
               return (
-                <li key={paramName}>
+                <li key={param.id}>
                   <div
                     data-cy="filter-menu-item"
                     className={c('drop__menu-item', {
                       'drop__menu-item--active': selected.parameters.includes(
-                        paramName
+                        param.id
                       ),
                     })}
                     data-hook="dropdown:close"
-                    onClick={() => onFilterSelect('parameters', paramName)}
+                    onClick={() => onFilterSelect('parameters', param.id)}
                   >
-                    <span data-cy={paramName}>{paramName}</span>
+                    <span data-cy={param.id}>{param.displayName}</span>
                   </div>
                 </li>
               );
@@ -322,16 +322,16 @@ export default function Filter({ countries, parameters, sources }) {
           })}
 
           {selected.parameters.map(o => {
-            const parameter = parameters.find(x => x.name === o);
+            const parameter = parameters.find(x => x.id === o);
             return (
               <button
                 type="button"
                 className="button--filter-pill"
                 data-cy="filter-pill"
-                key={parameter.name}
-                onClick={() => onFilterSelect('parameters', parameter.name)}
+                key={parameter.id}
+                onClick={() => onFilterSelect('parameters', parameter.id)}
               >
-                <span>{parameter.name}</span>
+                <span>{parameter.displayName}</span>
               </button>
             );
           })}
