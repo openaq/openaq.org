701fad23cde34cf19ad5528c4c4992f1823ac3e0
Jeevan Farias <jeevan@developmentseed.org>
2021-01-07 11:27:16 -0500

    Patch test

--- cypress/integration/locations-hub.spec.js
+++ cypress/integration/locations-hub.spec.js
@@ -49,7 +49,7 @@ describe('The Locations Hub', () => {
     // parameter filter
     cy.get('[title="type__filter"]').click();
     cy.get('[title="type__filter"]').find('span').contains('Parameter');
-    ['O3', 'CO', 'NO2', 'CO2', 'SO2', 'BC'].forEach(parameter => {
+    ['o3', 'co', 'no2', 'co2', 'so2', 'bc'].forEach(parameter => {
       cy.get('[data-cy=filter-parameters]')
         .find(`[data-cy=${parameter}]`)
         .should('length', 1);
