0da440d048cd527bb13446925f0edf6502c4b6af
Jeevan Farias <jeevan@developmentseed.org>
2021-01-12 13:32:45 -0500

    Implement all options for sensor type filter

--- app/assets/scripts/views/locations-hub/sensor-type-filter.js
+++ app/assets/scripts/views/locations-hub/sensor-type-filter.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import T from 'prop-types';
 import styled from 'Styled-Components';
+import c from 'classnames';
 
 const Wrapper = styled.div`
   width: 20rem;
 `;
 
-const Section = styled.div`
+const SectionWrapper = styled.div`
   border-bottom: 1px solid #ababab;
 `;
 const SectionTitle = styled.p``;
@@ -18,24 +19,74 @@ const SectionButtons = styled.div`
   }
 `;
 
+function Section(props) {
+  const { title, options, selected, setSelected } = props;
+  return (
+    <SectionWrapper>
+      <SectionTitle>{title}</SectionTitle>
+      <SectionButtons>
+        {options.map(v => (
+          <button
+            key={v}
+            type="button"
+            className={c('button', {
+              'button--primary': v === selected,
+              'button--primary-ghost': v !== selected,
+            })}
+            onClick={() => setSelected(v)}
+          >
+            {v}
+          </button>
+        ))}
+      </SectionButtons>
+      {props.children}
+    </SectionWrapper>
+  );
+}
+
 function SensorTypeFilter(props) {
   const { onApplyClick } = props;
+
+  const [selectedGrade, setSelectedGrade] = useState(null);
+  const [selectedMobility, setSelectedMobility] = useState(null);
+  const [selectedEntity, setSelectedEntity] = useState(null);
   return (
     <Wrapper>
-      <Section>
-        <SectionTitle></SectionTitle>
-        <SectionButtons>
-          {['Low Cost Sensor', 'Reference-Grade'].map(v => (
-            <button type="button" className="button--primary button">
-              {v}
-            </button>
-          ))}
-        </SectionButtons>
+      <Section
+        options={['Low Cost Sensor', 'Reference-Grade']}
+        selected={selectedGrade}
+        setSelected={setSelectedGrade}
+        title="Grade"
+      >
+        <select>
+          <option>option</option>
+        </select>
       </Section>
+
+      <Section
+        options={['Mobile', 'Stationary']}
+        selected={selectedMobility}
+        setSelected={setSelectedMobility}
+        title="Mobility"
+      />
+      <Section
+        options={['Community', 'Research', 'Government']}
+        selected={selectedEntity}
+        setSelected={setSelectedEntity}
+        title="Selected Entity"
+      />
 
       <button
         type="button"
-        className="button--primary button"
+        className={c('button', {
+          'button--base': !(
+            selectedGrade &&
+            selectedMobility &&
+            selectedEntity
+          ),
+          'button--primary':
+            selectedGrade && selectedMobility && selectedEntity,
+        })}
         onClick={onApplyClick}
       >
         Apply
@@ -44,5 +95,7 @@ function SensorTypeFilter(props) {
   );
 }
 
-SensorTypeFilter.propTypes = {};
+SensorTypeFilter.propTypes = {
+  onApplyClick: T.func,
+};
 export default SensorTypeFilter;
