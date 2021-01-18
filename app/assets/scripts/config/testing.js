f8fbc35d41de3300b8ccf1a3d9b15956312daa01
Necoline <necoline@developmentseed.org>
2021-01-18 09:54:06 -0500

    updates api addresses

--- app/assets/scripts/config/testing.js
+++ app/assets/scripts/config/testing.js
@@ -1,5 +1,5 @@
 module.exports = {
   environment: 'testing',
-  api: 'https://0jac6b9iac.execute-api.us-east-1.amazonaws.com/v2',
+  api: 'https://b4qc1cqcm2.execute-api.us-east-1.amazonaws.com/v2',
   metadata: 'https://metadata.openaq.org',
 };
