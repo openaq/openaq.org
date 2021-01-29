81214bae20079f30c560405a58f79e70ccd2213b
Alice Rühl <alice@developmentseed.org>
2021-01-29 11:22:50 +0100

    Update prod api

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -10,7 +10,7 @@ module.exports = {
     token: 'pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJnUi1mbkVvIn0.018aLhX0Mb0tdtaT2QNe2Q',
     baseStyle: 'mapbox://styles/devseed/ciqs29d060000clnr9222bg5x',
   },
-  api: 'https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2', // will eventually be 'https://api.openaq.org/v2'
+  api: 'https://api.openaq.org/v2',
   metadata: 'https://metadata.openaq.org',
   apiDocs: 'https://docs.openaq.org',
   feedbackUrl: 'https://forms.gle/rqsAPDSebNTG8PsJ8',
