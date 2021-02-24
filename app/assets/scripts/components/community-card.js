97605f90620cf36e09235f56f42d5e849220726d
Necoline <necoline@developmentseed.org>
2021-02-24 15:33:48 -0500

    add community card logos

--- app/assets/scripts/components/community-card.js
+++ app/assets/scripts/components/community-card.js
@@ -1,62 +1,72 @@
 import React from 'react';
 import { PropTypes as T } from 'prop-types';
-import createReactClass from 'create-react-class';
 
-/*
- * create-react-class provides a drop-in replacement for the outdated React.createClass,
- * see https://reactjs.org/docs/react-without-es6.html
- * Please modernize this code using functional components and hooks!
- */
-var CommunityCard = createReactClass({
-  displayName: 'CommunityCard',
+import pngImages from '../../graphics/content/view--community-projects/projects/logos/*.png';
+import jpgImages from '../../graphics/content/view--community-projects/projects/logos/*.jpg';
+import jpegImages from '../../graphics/content/view--community-projects/projects/logos/*.jpeg';
 
-  propTypes: {
-    title: T.string,
-    type: T.string,
-    location: T.string,
-    linkTitle: T.string,
-    url: T.string,
-    imageNode: T.node,
-    logo: T.string,
-    horizontal: T.bool,
-    children: T.object,
-  },
+import pngCoverImages from '../../graphics/content/view--community-projects/projects/covers/*.png';
+import jpgCoverImages from '../../graphics/content/view--community-projects/projects/covers/*.jpg';
+import jpegCoverImages from '../../graphics/content/view--community-projects/projects/covers/*.jpeg';
 
-  getDefaultProps: function () {
-    return {
-      horizontal: false,
-    };
-  },
+function getImage(logo) {
+  const filename = logo?.split('logos/')[1]?.split('.')[0];
+  const filenameCover = logo?.split('covers/')[1]?.split('.')[0];
+  return (
+    pngImages[filename] ||
+    jpgImages[filename] ||
+    jpegImages[filename] ||
+    pngCoverImages[filenameCover] ||
+    jpgCoverImages[filenameCover] ||
+    jpegCoverImages[filenameCover] ||
+    ''
+  );
+}
+export default function CommunityCard({
+  title,
+  type,
+  location,
+  linkTitle,
+  url,
+  imageNode,
+  logo,
+  horizontal,
+  children,
+}) {
+  console.log('logo', logo);
+  return (
+    <li>
+      <article className="card card--project">
+        <a href={url} className="card__contents" title="View project">
+          <header className="card__header">
+            <div className="card__headline">
+              <p className="card__subtitle">{type || 'N/A'}</p>
+              <h1 className="card__title">{title}</h1>
+            </div>
+          </header>
+          <div className="card__body">{children}</div>
+          <footer className="card__footer">
+            <p className="card__footer-detail">{location || 'N/A'}</p>
+            {logo && <img src={getImage(logo)} alt="Project logo" />}
+          </footer>
+        </a>
+      </article>
+    </li>
+  );
+}
 
-  render: function () {
-    return (
-      <li>
-        <article className="card card--project">
-          <a
-            href={this.props.url}
-            className="card__contents"
-            title="View project"
-          >
-            <header className="card__header">
-              <div className="card__headline">
-                <p className="card__subtitle">{this.props.type || 'N/A'}</p>
-                <h1 className="card__title">{this.props.title}</h1>
-              </div>
-            </header>
-            <div className="card__body">{this.props.children}</div>
-            <footer className="card__footer">
-              <p className="card__footer-detail">
-                {this.props.location || 'N/A'}
-              </p>
-              {this.props.logo && (
-                <img src={this.props.logo} alt="Project logo" />
-              )}
-            </footer>
-          </a>
-        </article>
-      </li>
-    );
-  },
-});
+CommunityCard.propTypes = {
+  title: T.string,
+  type: T.string,
+  location: T.string,
+  linkTitle: T.string,
+  url: T.string,
+  imageNode: T.node,
+  logo: T.string,
+  horizontal: T.bool,
+  children: T.object,
+};
 
-module.exports = CommunityCard;
+CommunityCard.defaultProps = {
+  horizontal: false,
+};
