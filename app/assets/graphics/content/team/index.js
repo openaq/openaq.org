63cbe1d4fe0d8e67211eb1c9a7b056d00ce928a9
Necoline <necoline@developmentseed.org>
2021-02-24 11:35:11 -0500

    adds index

---
+++ app/assets/graphics/content/team/index.js
@@ -0,0 +1,41 @@
+import Barciauskas from './avatar--aimee-barciauskas.jpg';
+import Sharma from './avatar--amrit-sharma.jpg';
+import Jairam from './avatar--arvind-jairam.jpg';
+import Calvert from './avatar--chisato-calvert.jpg';
+import Yoon from './avatar--heidi-yoon.jpg';
+import Taub from './avatar--jeremy-taub.jpg';
+import Seddon from './avatar--jessica-seddon.jpg';
+import Schultz from './avatar--martin-schultz.jpg';
+import Whitney from './avatar--matthew-whitney.png';
+import Brauer from './avatar--michael-brauer.jpg';
+import Veerman from './avatar--olaf-veerman.jpg';
+import Pant from './avatar--pallavi-pant.jpg';
+import Placeholder from './avatar--placeholder.jpg';
+import Shankar from './avatar--priya-shankar.jpg';
+import Wilson from './avatar--robin-wilson.jpg';
+import Lacombe from './avatar--romain-lacombe.jpg';
+import Guttikunda from './avatar--sarath-guttikunda.jpg';
+import Lee from './avatar--tom-lee.jpg';
+import Modekurty from './avatar--sruti-modekurty.jpg';
+
+export {
+  Barciauskas,
+  Sharma,
+  Jairam,
+  Calvert,
+  Yoon,
+  Taub,
+  Seddon,
+  Schultz,
+  Whitney,
+  Brauer,
+  Veerman,
+  Pant,
+  Placeholder,
+  Shankar,
+  Wilson,
+  Lacombe,
+  Guttikunda,
+  Lee,
+  Modekurty,
+};
