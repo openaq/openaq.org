80b9f472c774281ea7e9df8d1fe6d4419def646b
Dan Gorelick <dqgorelick@gmail.com>
2021-07-28 19:23:11 -0400

    adds copy updates for site

--- app/assets/scripts/components/ambassador-card.js
+++ app/assets/scripts/components/ambassador-card.js
@@ -9,7 +9,9 @@ export default function AmbassadorCardDetails({ youtubeEmbedId, linkedIn }) {
       {linkedIn ? (
         <div>
           <p>
-            <a href={linkedIn}>LinkedIn</a>
+            <a target="_blank" rel="noopener noreferrer" href={linkedIn}>
+              LinkedIn
+            </a>
           </p>
         </div>
       ) : (
