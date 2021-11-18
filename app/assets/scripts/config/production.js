8532ba3262fab1f2b6cdfff8faee202758891a89
Russ Biggs <russbiggs@gmail.com>
2021-11-18 08:21:21 -0700

    update mapbox credentials

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -7,8 +7,8 @@ module.exports = {
   environment: 'production',
   consoleMessage: logo,
   mapbox: {
-    token: 'pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJnUi1mbkVvIn0.018aLhX0Mb0tdtaT2QNe2Q',
-    baseStyle: 'mapbox://styles/devseed/ciqs29d060000clnr9222bg5x',
+    token: 'pk.eyJ1Ijoib3BlbmFxIiwiYSI6ImNrdzJmc2drMDBhcmgydW1wYTQ5aTVhNHgifQ.aY3RxfSZuCApSitdjdviQQ',
+    baseStyle: 'mapbox://styles/openaq/ckw2fwkcc1o5314qs5l555iip',
   },
   api: 'https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2', // will eventually be 'https://api.openaq.org/v2'
   metadata: 'https://metadata.openaq.org',
