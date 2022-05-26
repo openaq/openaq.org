6757b66e6060093d436cb4bf4a0c94f5b7a30088
Russ Biggs <russbiggs@gmail.com>
2022-05-26 12:19:40 -0600

    update api url

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -11,7 +11,7 @@ module.exports = {
       'pk.eyJ1Ijoib3BlbmFxIiwiYSI6ImNrdzJmc2drMDBhcmgydW1wYTQ5aTVhNHgifQ.aY3RxfSZuCApSitdjdviQQ',
     baseStyle: 'mapbox://styles/openaq/ckw2fwkcc1o5314qs5l555iip',
   },
-  api: 'https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2', // will eventually be 'https://api.openaq.org/v2'
+  api: 'https://api.openaq.org/v2',
   metadata: 'https://metadata.openaq.org',
   apiDocs: 'https://docs.openaq.org',
   feedbackUrl: 'https://forms.gle/rqsAPDSebNTG8PsJ8',
