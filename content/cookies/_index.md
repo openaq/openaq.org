0ff3c335d95d9da98986c35be336f289fbc09bd7
Gabriel Fosse <67290377+majesticio@users.noreply.github.com>
2023-01-13 09:08:56 -0700

    plausible footer goals

--- content/cookies/_index.md
+++ content/cookies/_index.md
@@ -4,4 +4,4 @@ title = "Cookie Policy"
 
 ## OpenAQ.org
 
-OpenAQ uses [Plausible Analytics](https://plausible.io/) to track overall trends in the usage of our website. Cookies are not used or stored by Plausible analytics. For more information, see the [Plausible Data Policy](https://plausible.io/data-policy)
+OpenAQ uses [Plausible Analytics](https://plausible.io/) to track overall trends in the usage of our website. Cookies are not used or stored by Plausible Analytics. For more information, see the [Plausible Data Policy](https://plausible.io/data-policy)
--- themes/openaq/assets/js/plausible-base.js
+++ themes/openaq/assets/js/plausible-base.js
@@ -3,4 +3,21 @@
     headerDonate.addEventListener('click', () => plausible('onClickHeaderDonate'));
     const headerDocsLink = document.querySelector('.js-header-docs-link');
     headerDocsLink.addEventListener('click', () => plausible('onClickHeaderDocsLink'));
+    // footer section
+    const footerTwitterLink = document.querySelector('.js-footer-twitter-link');
+    footerTwitterLink.addEventListener('click', () => plausible('onClickFooterTwitterLink'));
+    const footerGithubLink = document.querySelector('.js-footer-github-link');
+    footerGithubLink.addEventListener('click', () => plausible('onClickFooterGithubLink'));
+    const footerSlackLink = document.querySelector('.js-footer-slack-link');
+    footerSlackLink.addEventListener('click', () => plausible('onClickFooterSlackLink'));
+    const footerLinkedInLink = document.querySelector('.js-footer-linkedIn-link');
+    footerLinkedInLink.addEventListener('click', () => plausible('onClickFooterLinkedInLink'));
+    const footerMediumLink = document.querySelector('.js-footer-medium-link');
+    footerMediumLink.addEventListener('click', () => plausible('onClickFooterMediumLink'));
+    const footerYoutubeLink = document.querySelector('.js-footer-youtube-link');
+    footerYoutubeLink.addEventListener('click', () => plausible('onClickFooterYoutubeLink'));
+    const footerFacebookLink = document.querySelector('.js-footer-fb-link');
+    footerFacebookLink.addEventListener('click', () => plausible('onClickFooterFacebookLink'));
+    const footerSubscribe = document.querySelector('.js-footer-subscribe');
+    footerSubscribe.addEventListener('click', () => plausible('onClickFooterSubscribe'));
 })();
