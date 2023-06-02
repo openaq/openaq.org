c57367dceb207b6baee37488d36c9031cd2b46cf
Russ Biggs <russbiggs@gmail.com>
2023-06-02 14:39:02 -0600

    add security.txt (#876)

---
+++ .github/scripts/update_security.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+import re
+
+with open("./security.txt", "r+") as f:
+    data = f.read()
+    dt = datetime.now()
+    if dt.month == 1:
+        new_date = datetime(
+            year=dt.year, month=7, day=31, hour=11, minute=59, second=59
+        )
+    if dt.month == 6:
+        new_date = datetime(
+            year=dt.year + 1, month=1, day=31, hour=11, minute=59, second=59
+        )
+    expire_line = f"Expires: {new_date.isoformat()}Z\n"
+    new_data = re.sub(
+        r"Expires\:\s\d{4}\-\d{2}\-\d{2}T\d{2}\:\d{2}\:\d{2}Z\n", expire_line, data
+    )
+    f.seek(0)
+    f.write(new_data)
