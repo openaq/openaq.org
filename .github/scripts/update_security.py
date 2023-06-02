ad22cbf3b2d25adf172e0f908aae9246a693dc12
Russ Biggs <russbiggs@gmail.com>
2023-06-02 14:57:29 -0600

    update security script

--- .github/scripts/update_security.py
+++ .github/scripts/update_security.py
@@ -8,7 +8,7 @@ with open("themes/openaq/static/.well-known/security.txt", "r+") as f:
         new_date = datetime(
             year=dt.year, month=7, day=31, hour=11, minute=59, second=59
         )
-    if dt.month == 6:
+    if dt.month == 7:
         new_date = datetime(
             year=dt.year + 1, month=1, day=31, hour=11, minute=59, second=59
         )
