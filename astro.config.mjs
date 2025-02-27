1d1b94ea85b2114927047081a2b832096db1e9b4
Russ Biggs <russbiggs@gmail.com>
2025-02-27 14:19:38 -0700

    purgecss and landing page images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,20 @@
 import { defineConfig } from 'astro/config';
 import { remarkModifiedTime } from './remark-modified-time.mjs';
 
+import purgecss from 'astro-purgecss';
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   output: 'static',
+
   experimental: {
     svg: true,
   },
+
   markdown: {
     remarkPlugins: [remarkModifiedTime],
   },
+
+  integrations: [purgecss()],
 });
