73208e24584043e650c58c53395beca54dc520fa
Helena Skagerlid <helenaskagerlid@gmail.com>
2025-03-14 16:43:36 +0100

    wip og endpoint

--- src/pages/og/[...slug].ts
+++ src/pages/og/[...slug].ts
@@ -1,26 +1,42 @@
 import { getCollection } from "astro:content";
 import { OGImageRoute } from "astro-og-canvas";
 
-const entries = await getCollection("advisors");
+const staff = await getCollection("staff");
+const ambassadors = await getCollection("ambassadors");
+const initiatives = await getCollection("initiatives");
+const policies = await getCollection("policies");
+const usecases = await getCollection("usecases");
 
-const pages = Object.fromEntries(entries.map(({ data, id }) => [id, { data }]));
+const allCollections = [
+  ...staff,
+  ...ambassadors,
+  ...initiatives,
+  ...policies,
+  ...usecases,
+];
+
+const pages = Object.fromEntries(
+  allCollections.map(({ id, data }) => [
+    id,
+    { title: data.title, description: "Testing" },
+  ])
+);
 
 export const { getStaticPaths, GET } = OGImageRoute({
   pages,
   param: "slug",
   getImageOptions: (_path, page: (typeof pages)[number]) => {
     return {
-      title: `OpenAQ-${page.data.title}`,
-      description: page.data.position,
+      title: `OpenAQ-${page.title}`,
+      description: page.description,
       bgGradient: [
-        [30, 100, 171],
         [106, 92, 216],
+        [227, 224, 251],
       ],
       logo: {
-        path: "@assets/svg/logo.svg",
-        size: [200],
+        path: "./src/assets/logo.svg",
       },
-      border: { color: [63, 194, 192], width: 20 },
+      border: { color: [106, 92, 216], width: 20 },
       padding: 40,
     };
   },
