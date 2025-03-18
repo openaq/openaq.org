4167e962f44e1453fe815d36329f9a310d1ffb1d
Helena Skagerlid <helenaskagerlid@gmail.com>
2025-03-18 12:30:21 +0100

    wip added dynamic og content to collections

--- src/pages/og/[...slug].ts
+++ src/pages/og/[...slug].ts
@@ -7,18 +7,58 @@ const initiatives = await getCollection("initiatives");
 const policies = await getCollection("policies");
 const usecases = await getCollection("usecases");
 
+const staffContent = staff.map((person) => {
+  return {
+    id: person.id,
+    title: person.data.title,
+    description: `${person.data.title} - ${person.data.position}`,
+  };
+});
+
+const ambassadorsContent = ambassadors.map((person) => {
+  return {
+    id: person.id,
+    title: person.data.title,
+    description: `${person.data.title} - ${person.data.position}`,
+  };
+});
+
+const usecasesContent = usecases.map((usecase) => {
+  return {
+    id: usecase.id,
+    title: usecase.data.title,
+    description: `Explore the OpenAQ usecase: "${usecase.data.title}"`,
+  };
+});
+
+const policiesContent = policies.map((policy) => {
+  return {
+    id: policy.id,
+    title: policy.data.title,
+    description: `Read about OpenAQ's ${policy.data.title}`,
+  };
+});
+
+const initiativesContent = initiatives.map((initiative) => {
+  return {
+    id: initiative.id,
+    title: initiative.data.title,
+    description: `Explore the OpenAQ intitiative: "${initiative.data.title}"`,
+  };
+});
+
 const allCollections = [
-  ...staff,
-  ...ambassadors,
-  ...initiatives,
-  ...policies,
-  ...usecases,
+  ...staffContent,
+  ...ambassadorsContent,
+  ...initiativesContent,
+  ...usecasesContent,
+  ...policiesContent,
 ];
 
 const pages = Object.fromEntries(
-  allCollections.map(({ id, data }) => [
+  allCollections.map(({ id, title, description }) => [
     id,
-    { title: data.title, description: "Testing" },
+    { title, description },
   ])
 );
 
@@ -27,7 +67,7 @@ export const { getStaticPaths, GET } = OGImageRoute({
   param: "slug",
   getImageOptions: (_path, page: (typeof pages)[number]) => {
     return {
-      title: `OpenAQ-${page.title}`,
+      title: `OpenAQ`,
       description: page.description,
       bgGradient: [
         [106, 92, 216],
